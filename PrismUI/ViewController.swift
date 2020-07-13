42dbe7a955b15b577687e7609f078a8e64397448
ErrorErrorError <erikbautista15@gmail.com>
2020-07-13 17:31:38 -0500

    Initial Commit

---
+++ PrismUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PrismUI
+//
+//  Created by Erik Bautista on 7/13/20.
+//  Copyright © 2020 ErrorErrorError. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
